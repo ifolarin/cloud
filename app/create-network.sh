65f3c105c059e586b4f02e682c1176bce3680133
Ibrahim Folarin <ifolarin@claritae.com>
2025-08-13 19:26:16 +0100

    fix -scope error

--- app/create-network.sh
+++ app/create-network.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker network inspect app_frontend_network >/dev/null 2>&1 || docker network create --driver overlay -scope swarm app_frontend_network
-docker network inspect app_backend_network >/dev/null 2>&1 || docker network create --driver overlay -scope swarm app_backend_network
+docker network inspect frontend >/dev/null 2>&1 || docker network create --driver overlay --scope swarm frontend
+docker network inspect enumverse_backend_network >/dev/null 2>&1 || docker network create --driver overlay --scope swarm enumverse_backend_network
