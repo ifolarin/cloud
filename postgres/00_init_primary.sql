5c0cd69e6aba4532ea1a08ce78bd3b0269daba9c
Ibrahim Folarin <ibrahim@labsbysemicolon.com>
2025-08-16 22:29:27 +0100

    pgpool2 loadbalancer done

--- postgres/00_init_primary.sql
+++
@@ -1,3 +0,0 @@
-create user replicator with replication encrypted password 'replicator_password';
-select pg_create_physical_replication_slot('replication_slot_1');
-select pg_create_physical_replication_slot('replication_slot_2');
