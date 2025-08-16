56704f9020d31d76ba92bbe56b840c0c39011e8f
Ibrahim Folarin <ibrahim@labsbysemicolon.com>
2025-08-16 17:58:02 +0100

    Primary and 2 replica done

--- postgres/create-data-dir.sh
+++ postgres/create-data-dir.sh
@@ -4,6 +4,7 @@ source ./.env
 set +a
 PGUID=70
 PGGID=70
-mkdir -p ${DATA_VOLUME?[ERROR] ENV VAR DATA_VOLUME NOT SET}/postgres/primary/data
-mkdir -p ${DATA_VOLUME?[ERROR] ENV VAR DATA_VOLUME NOT SET}/postgres/replica/data
+mkdir -p ${DATA_VOLUME?[ERROR] ENV VAR DATA_VOLUME NOT SET}/postgres/primary/pgdata
+mkdir -p ${DATA_VOLUME?[ERROR] ENV VAR DATA_VOLUME NOT SET}/postgres/replica-1/pgdata
+mkdir -p ${DATA_VOLUME?[ERROR] ENV VAR DATA_VOLUME NOT SET}/postgres/replica-2/pgdata
 chown -R $PGUID:$PGGID ${DATA_VOLUME?[ERROR] ENV VAR DATA_VOLUME NOT SET}/postgres/
