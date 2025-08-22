7ddd3d510f053d1ab852b78b58230f7662627cc5
Ibrahim Folarin <ibrahim@labsbysemicolon.com>
2025-08-22 14:20:34 +0100

    Init initial database  and user on startup done

--- mongodb/conf/00_init_mongodb.sh
+++ mongodb/conf/00_init_mongodb.sh
@@ -56,5 +56,33 @@ var config = {
 };
 rs.initiate(config, { force: true });
 rs.status();
-use ${MONGO_INITDB_DATABASE}
+EOF
+
+echo "###### Waiting for Primary election.."
+until mongosh --host ${m1}:${port} --eval 'quit(db.isMaster().ismaster ? 0 : 2)' &>/dev/null; do
+  printf '.'
+  sleep 5
+done
+
+echo "###### Primary elected. Initiating initial db and user setup..."
+mongosh --host ${m1}:${port} <<EOF
+var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+var admin = db.getSiblingDB('admin');
+admin.auth(rootUser, rootPassword);
+
+var db = db.getSiblingDB('${MONGO_INITDB_DATABASE}');
+show dbs;
+db.createUser(
+  {
+    user: "${MONGO_USERNAME}", 
+    pwd: "${MONGO_PASSWORD}", 
+    roles :[ 
+      {role: "readWrite", db: "${MONGO_INITDB_DATABASE}"}
+    ]
+  }
+);
+show users;
+db.createCollection("initCollection");
+db.initCollection.insertOne({ name: "Enumverse Document", value: 123 });
 EOF
--- mongodb/deploy.sh
+++ mongodb/deploy.sh
@@ -5,4 +5,4 @@ source ./.env
 set +a # disable autoatic export
 CONFIG="stack.yml"
 if [ ! -f $CONFIG ]; then CONFIG="docker-compose.yml"; fi # shellcheck source=path/to/file
-docker stack deploy -c $CONFIG mongodb_cluster
+docker stack deploy -c $CONFIG mg_cluster
